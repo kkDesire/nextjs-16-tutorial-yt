193093c94395d7f2de555f830c95ee01b6d08362
Desire KOUASSI <kouamdesire90@gmail.com>
2025-12-14 00:07:12 +0000

    feat: Add `/api/create-blog` POST endpoint.

---
+++ app/api/create-blog/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+export async function POST(){
+    console.log("Hello from the server")
+
+    return NextResponse.json({
+        success: true,
+    })
+}
